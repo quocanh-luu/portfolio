63a75470b148391984f92e9d6c1964b4f820c70d
quocanh-luu <anh.qluu03@gmail.com>
2025-10-22 21:01:31 +0200

    Upload project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { HeroComponent } from "./components/hero/hero.component";
+import { AboutComponent } from './components/about/about.component';
+import { Project1Component } from './components/project1/project1.component';
+import { Project2Component } from './components/project2/project2.component';
+import { Project3Component } from './components/project3/project3.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, HeroComponent, AboutComponent, Project1Component, Project2Component, Project3Component, ContactComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
